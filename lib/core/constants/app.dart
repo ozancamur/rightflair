bec81ba24cccaf420ffa16b86fa82c710c7f2651
Ozan Camur <72649916+ozancamur@users.noreply.github.com>
2026-02-09 23:58:52 +0300

    Update app.dart

--- lib/core/constants/app.dart
+++ lib/core/constants/app.dart
@@ -1,7 +1,7 @@
 class AppConstants {
   const AppConstants._();
 
-  static const String APP_NAME = 'RightFlair';
+  static const String APP_NAME = 'Rightflair';
   static const String PATH_LOCALIZATION = 'assets/translations';
   static const String VERSION = '1.0.0';
 
