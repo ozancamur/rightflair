92e9ef99d8ed6f39aa06adeeae5dae1c5e166623
Ozan Camur <72649916+ozancamur@users.noreply.github.com>
2026-02-14 22:22:38 +0300

    remove blur face func

--- lib/core/services/api.dart
+++ lib/core/services/api.dart
@@ -1,6 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
-import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:rightflair/core/services/supabase.dart';
 import 'package:rightflair/core/constants/enums/endpoint.dart';
 
@@ -38,21 +36,6 @@ class ApiService extends SupabaseService {
         },
       ),
     );
-
-    // İsteğe bağlı olarak interceptor'lar eklenebilir
-    if (kDebugMode) {
-      dio.interceptors.add(
-        PrettyDioLogger(
-          requestHeader: true,
-          requestBody: true,
-          responseBody: true,
-          responseHeader: false,
-          error: true,
-          compact: true,
-          maxWidth: 90,
-        ),
-      );
-    }
   }
 
   // GET Method
--- lib/feature/post/create_post/cubit/create_post_cubit.dart
+++ lib/feature/post/create_post/cubit/create_post_cubit.dart
@@ -9,11 +9,9 @@ import 'package:rightflair/core/constants/string.dart';
 import 'package:rightflair/core/utils/dialogs/error.dart';
 import 'package:rightflair/feature/post/create_post/model/create_post.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import '../model/blur.dart';
 import '../model/mention_user.dart';
 import '../model/music.dart';
 import '../repository/create_post_repository.dart';
-import '../../../../core/utils/face_blur.dart';
 
 part '../widgets/create_post_state.dart';
 
@@ -35,38 +33,8 @@ class CreatePostCubit extends Cubit<CreatePostState> {
   Future<void> toggleAnonymous(BuildContext context, bool value) async {
     if (state.originalImagePath != null) {
       emit(state.copyWith(isProcessingImage: true));
-      final response = await _processImageWithBlur(
-        state.originalImagePath!,
-        isAnonymous: value,
-      );
-      if (response.isBlurred == false) {
-        dialogError(context, message: "Yüz algılama başarısız oldu.");
-      }
-      emit(
-        state.copyWith(
-          isAnonymous: response.isBlurred,
-          imagePath: response.file.path,
-          isProcessingImage: false,
-        ),
-      );
+      emit(state.copyWith(isProcessingImage: false));
     }
-  }
-
-  Future<BlurModel> _processImageWithBlur(
-    String imagePath, {
-    required bool isAnonymous,
-    int blurRadius = 100,
-  }) async {
-    if (!isAnonymous) {
-      return BlurModel(file: File(imagePath), isBlurred: false);
-    }
-
-    final originalFile = File(imagePath);
-    final blurredFile = await blurFacesInImage(
-      originalFile,
-      blurRadius: blurRadius,
-    );
-    return blurredFile;
   }
 
   void toggleAllowComments(bool value) =>
@@ -124,33 +92,16 @@ class CreatePostCubit extends Cubit<CreatePostState> {
     );
     if (image != null) {
       emit(state.copyWith(isProcessingImage: true));
-      final response = await _processImageWithBlur(
-        image.path,
-        isAnonymous: state.isAnonymous,
-      );
       emit(
-        state.copyWith(
-          imagePath: response.file.path,
-          originalImagePath: image.path,
-          isProcessingImage: false,
-        ),
+        state.copyWith(originalImagePath: image.path, isProcessingImage: false),
       );
     }
   }
 
   Future<void> setImagePath(String path) async {
     emit(state.copyWith(isProcessingImage: true));
-    final response = await _processImageWithBlur(
-      path,
-      isAnonymous: state.isAnonymous,
-    );
-    emit(
-      state.copyWith(
-        imagePath: response.file.path,
-        originalImagePath: path,
-        isProcessingImage: false,
-      ),
-    );
+
+    emit(state.copyWith(originalImagePath: path, isProcessingImage: false));
   }
 
   Future<void> createPost(
